71723880a4eed08f7c5e53ff80d7b2d5eaef8fb5
prog-ammar <ammarofc2005@gmail.com>
2025-09-03 22:12:57 +0500

    updated

--- src/client.rs
+++ src/client.rs
@@ -9,6 +9,9 @@ fn connect(server_ip: &str)
     let mut server=TcpStream::connect(server_ip).unwrap();
     let mut name=String::new();
 
+    let reader_stream = server.try_clone().expect("Failed to clone stream");
+    let writer_stream = server.try_clone().expect("Failed to clone stream");
+
     let mut recv_data=[0;1024];
 
     match server.read(&mut recv_data)
@@ -41,10 +44,12 @@ fn connect(server_ip: &str)
         }
     }
 
-    let handle=thread::spawn(||
+    let _handle=thread::spawn(move||
     {
-
-        let mut rec_data=[0;1024];
+        let mut server=reader_stream;
+        loop
+        {
+            let mut rec_data=[0;1024];
 
         match server.read(&mut rec_data)
         {
@@ -55,14 +60,39 @@ fn connect(server_ip: &str)
             }
             Ok(_) =>
             {
-                println!("Connection Closed");
+                println!("idk");
             }
             Err(e) =>
             {
-                panic!("Error : {}",e)
+                println!("Error : {}",e);
             }
-        }   
+        }  
+        }
     });
+    
+
+    let _new_handle=thread::spawn(move||
+    {
+        let mut server=writer_stream;
+        let mut message=String::new();
+        loop
+        {
+            io::stdin().read_line(&mut message).expect("Error has occured");
+            match server.write_all(message.trim().as_bytes())
+            {
+                Ok(_) =>
+                {
+                    
+                }
+                Err(e) =>
+                {
+                    println!("Error : {}",e);
+                }
+            }  
+                
+        }  
+    });
+
 }
 
 fn main()
