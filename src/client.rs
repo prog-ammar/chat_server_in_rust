c725107ce01e7d9c78772a429b15c0f63b5a4f6e
prog-ammar <ammarofc2005@gmail.com>
2025-09-04 06:38:46 +0500

    added some functionality

--- src/client.rs
+++ src/client.rs
@@ -6,36 +6,58 @@ use std::thread;
 
 fn connect(server_ip: &str)
 {
-    let mut server=TcpStream::connect(server_ip).unwrap();
+    let mut server=TcpStream::connect(server_ip).expect("Failed To Connect");
 
     let reader_stream = server.try_clone().expect("Failed to clone stream");
     let writer_stream = server.try_clone().expect("Failed to clone stream");
     
     let mut name=String::new();
 
+    // server.set_nonblocking(true).unwrap();
+    // reader_stream.set_nonblocking(true).unwrap();
+    // writer_stream.set_nonblocking(true).unwrap();
+
     let mut recv_data=[0;1024];
 
     let mut first_time_connect:bool = false;
-    match server.read(&mut recv_data)
+
+    let mut buf=[0;1];
+
+    match server.peek(&mut buf)
     {
-        Ok(bytes) if bytes > 0 =>
+        Ok(0) => 
         {
-            let received = String::from_utf8_lossy(&recv_data[..bytes]);
-            println!("{}",received);
-            first_time_connect=true;
+            println!("Server Disconnected!");
         }
-        Ok(_) =>
+        Ok(_) => 
         {
-            println!("Connection Closed");
+            println!("Some Data is Avaliable");
+            first_time_connect=true;
         }
         Err(e) =>
         {
-            panic!("Error : {}",e)
+            println!("Error : {}",e);
         }
     }
     
     if first_time_connect==true
     {
+        match server.read(&mut recv_data)
+        {
+            Ok(bytes) if bytes > 0 =>
+            {
+                let received = String::from_utf8_lossy(&recv_data[..bytes]);
+                println!("{}",received);
+            }
+            Ok(_) =>
+            {
+                println!("Connection Closed");
+            }
+            Err(e) =>
+            {
+                panic!("Error : {}",e)
+            }
+        }
         io::stdin().read_line(&mut name).expect("Error has occured");
         match server.write_all(name.trim().as_bytes())
         {
@@ -53,26 +75,48 @@ fn connect(server_ip: &str)
     let _handle=thread::spawn(move||
     {
         let mut server=reader_stream;
+        let mut rec_data=[0;1024];
+        let mut buffer=[0;1];
+        let mut data_present:bool=false;
         loop
         {
-            let mut rec_data=[0;1024];
 
-        match server.read(&mut rec_data)
-        {
-            Ok(bytes) if bytes > 0 =>
+            match server.peek(&mut buffer)
             {
-                let received = String::from_utf8_lossy(&rec_data[..bytes]);
-                println!("{}",received);
+                Ok(0) => 
+                {
+                    data_present=false;
+                }
+                Ok(_) => 
+                {
+                    data_present=true;
+                }
+                Err(e) =>
+                {
+                    println!("Error : {}",e);
+                }
             }
-            Ok(_) =>
+
+            if data_present
             {
-                println!("idk");
+                match server.read(&mut rec_data)
+                {
+                    Ok(bytes) if bytes > 0 =>
+                    {
+                        let received = String::from_utf8_lossy(&rec_data[..bytes]);
+                        println!("{}",received);
+                    }
+                    Ok(_) =>
+                    {
+                        println!("idk");
+                    }
+                    Err(e) =>
+                    {
+                        println!("Error : {}",e);
+                    }
+                }  
             }
-            Err(e) =>
-            {
-                println!("Error : {}",e);
-            }
-        }  
+        
         }
     });
     
